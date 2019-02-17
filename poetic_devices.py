9f2055fa7e874eef05804619e997387d2ac1620c
Arun Ajay <arunajay42795@gmail.com>
2019-02-17 04:53:02 -0500

    finished alliteration code

--- poetic_devices.py
+++ poetic_devices.py
@@ -22,9 +22,43 @@ def extract_transcribed_words_per_line(poem_lines):
         transcribed_lines.append(transcribed_line)
     return transcribed_lines
 
+def dict_create_one_line(line1, line2):
+	consonant_list = ["b","d","f","g","h","dʒ","k","l","m","n","p","r","s",
+"t","v","w","z","ʒ","tʃ","ʃ","θ","ð","ŋ","j"]
+	word_dict = {}
+	for word,index in zip(line1, line2):
+		if word[:1].lower() == "d":
+			if word[:2].lower() == "dʒ":
+				word_dict.setdefault("dʒ", []).append(index)
+			word_dict.setdefault("dʒ", []).append(index)
+			continue
+		if word[:1].lower() == "t":
+			if word[:2].lower() == "tʃ":
+				word_dict.setdefault("tʃ", []).append(index)
+			word_dict.setdefault("t", []).append(index)
+			continue
+		if word[:1].lower() in consonant_list:
+			word_dict.setdefault(word[:1].lower(), []).append(index)
+	return word_dict
 
-def alliteration():
-    print("hi")
+
+
+def alliteration(word_list_per_line, transcribe_list):
+	dictionary_list = [ dict_create_one_line(line,line2) for line,line2 in zip(transcribe_list,word_list_per_line)]
+	print(dictionary_list)
+
+	main_list = []
+	for each_dict in dictionary_list:
+		sub_list = []
+		for wl in each_dict.values():
+			if len(wl) >= 2:
+				for word in wl:
+					sub_list.append(word)
+		if len(sub_list) >= 2:
+			main_list.append(sub_list)
+
+	print(main_list)
+
 
 
 def onomatopoeia(poem_lines):
@@ -52,11 +86,13 @@ transcribe_list = extract_transcribed_words_per_line(poem_lines)
 for word1,word2 in zip(word_list_per_line[0],transcribe_list[0]):
 	print(word1,word2)
 
-ono_list = onomatopoeia(word_list_per_line)
+alliteration(word_list_per_line,transcribe_list)
 
-for word in ono_list:
-	print(word)
 
-consonant_list = ["b","d","f","g","h","dʒ","k","l","m","n","p","r","s",
-"t","v","w","z","ʒ","tʃ","ʃ","θ","ð","ŋ","j"]
-print(len(consonant_list))
+
+
+# ono_list = onomatopoeia(word_list_per_line)
+
+# for word in ono_list:
+# 	print(word)
+
