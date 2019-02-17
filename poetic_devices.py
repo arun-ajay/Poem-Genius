832a28f5ecdf7442bbdf2e5744178ae37ad78d19
Arun Ajay <arunajay42795@gmail.com>
2019-02-16 20:38:17 -0500

    added function to read poems and split line by line

--- poetic_devices.py
+++ poetic_devices.py
@@ -1,3 +1,18 @@
 #!/usr/bin/env python3
 
-def alliteration(
+def getfile(poem_txt):
+    poem_lines = [line.rstrip('\n') for line in open (poem_txt)]
+    return poem_lines
+def print_poem(poem_lines):
+	for line in poem_lines:
+		print(line)
+
+
+
+file = input("Give me a file name: ")
+poem_lines = getfile(file)
+print_poem(poem_lines)
+
+
+
+
